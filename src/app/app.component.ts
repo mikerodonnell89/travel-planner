79da465a8cd7a3b1d168508562f46adf86094301
Mike O'Donnell <mikerodonnell89@users.noreply.github.com>
2020-08-24 15:20:31 -0600

    finished app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FdDate, FdRangeDate} from '@fundamental-ngx/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'travel-planner';
+  tableRows = [];
+  dates: FdRangeDate;
+  destination;
+  notes;
+  showForm = false;
+  files: FileList;
+
+  submitTrip() {
+    this.tableRows.push({
+      dates: this.dates.start.toDateString() + ' - ' + this.dates.end.toDateString(),
+      destination: this.destination,
+      notes: this.notes,
+      files: this.files
+    });
+    this.dates = {
+      start: FdDate.getToday(),
+      end: FdDate.getToday().nextDay()
+    };
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.showForm = false;
+    this.destination = '';
+    this.notes = '';
+    this.files = null;
+  }
+
+  download(file) {
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(file);
+    a.download = file.name;
+    a.click();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {FundamentalNgxCoreModule, ToolbarModule} from '@fundamental-ngx/core';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    FundamentalNgxCoreModule,
+    ToolbarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
